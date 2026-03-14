a1fccbd5bbdf33254393fdb0bd465606285dffbe
= <=>
2026-03-14 09:46:15 -0300

    feat:user authentication

--- webapi/Data/AppDbContext.cs
+++ webapi/Data/AppDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using webapi.Models;
 
 namespace webapi.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<IdentityUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
@@ -12,5 +14,42 @@ namespace webapi.Data
         public DbSet<Role> Roles { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<UserSession> UserSessions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Role>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Description).IsRequired();
+            });
+
+            builder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Username).IsRequired();
+                entity.Property(e => e.PasswordHash).IsRequired();
+                entity.HasOne<Role>()
+                      .WithMany()
+                      .HasForeignKey(e => e.RoleId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            builder.Entity<UserSession>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+
+                entity.HasIndex(e => e.Token).HasDatabaseName("IX_UserSession_Token");
+
+                entity.HasOne<IdentityUser>()
+                      .WithMany()
+                      .HasForeignKey(e => e.UserId)
+                      .OnDelete(DeleteBehavior.Cascade);
+
+                entity.Property(e => e.Token).IsRequired();
+                entity.Property(e => e.Expiration).IsRequired();
+            });
+        }
     }
 }
--- webapi/Models/User.cs
+++ webapi/Models/User.cs
@@ -6,5 +6,8 @@
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public string Username { get; set; }
+        public int RoleId { get; set; }
+        public string PasswordHash { get; set; }
     }
 }
